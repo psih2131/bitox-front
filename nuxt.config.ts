2ad18c6c2be92b8309918a659aeac6b5de9b224f
psih2131 <gearsdevelope@gmail.com>
2026-07-15 15:21:50 +0300

    add yandex counter script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,19 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/png', href: '/favicon.png' },
       ],
+      noscript: [
+        {
+          key: 'yandex-metrika',
+          innerHTML: '<div><img src="https://mc.yandex.ru/watch/110762000" style="position:absolute; left:-9999px;" alt="" /></div>',
+        },
+      ],
       script: [
+        {
+          key: 'yandex-metrika',
+          type: 'text/javascript',
+          tagPosition: 'head',
+          innerHTML: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=110762000','ym');ym(110762000,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});`,
+        },
         {
           key: 'chatapp-widget',
           type: 'text/javascript',
