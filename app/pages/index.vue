4801b459a00e1279b963626b7f40e1a1fdbcd407
psih2131 <gearsdevelope@gmail.com>
2026-07-26 20:57:02 +0300

    fix

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -20,11 +20,11 @@
 
     <HomeTeamSec v-if="home?.home_team_sec" :section="home.home_team_sec" />
 
-    <!-- <HomeHowStartSec v-if="home?.home_how_start_sec" :section="home.home_how_start_sec" /> -->
+    <HomeHowStartSec v-if="home?.home_how_start_sec" :section="home.home_how_start_sec" />
 
     <HomeFaqSec v-if="home?.home_faq_sec" :section="home.home_faq_sec" />
 
-    <HomeCtaSec v-if="home?.home_down_ctr_v1_sec" :section="home.home_down_ctr_v1_sec" />
+    <!-- <HomeCtaSec v-if="home?.home_down_ctr_v1_sec" :section="home.home_down_ctr_v1_sec" /> -->
   </main>
 </template>
 
